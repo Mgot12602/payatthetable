68c50bbd4a39db55c24db1b8af7aa6e38b59159e
Marc Gotzens <marcgotzens@hotmail.com>
2020-12-14 10:24:17 +0100

    made order work in the part of adding dishes and increasing units

--- routes/order.js
+++ routes/order.js
@@ -42,7 +42,7 @@ router.post("/addDishToOrder", (req, res, next) => {
   const { dishId, table } = req.body;
   console.log(`This is the DishId ${dishId}, this is the table ${table}`);
   Order.find({ table: req.body.table }).then((foundOrder) => {
-    console.log("this is the found order id", foundOrder[0]._id);
+    console.log("this is the found order", foundOrder[0]);
     // console.log("this is the found order object", foundOrder[0]);
     // if (foundOrder[0].dishesOrdered.includes(dishId)) {
     //   console.log("it says that it already includes the dish");
@@ -52,17 +52,37 @@ router.post("/addDishToOrder", (req, res, next) => {
 
     console.log("dishId", dishId);
 
-    const exists = foundOrder[0].dishesOrdered.reduce((acc, el) => {
-      if (el.dishType == dishId) {
-        return (acc = true);
+    // const exists = foundOrder[0].dishesOrdered.reduce((acc, el) => {
+    //   if (el.dishType == dishId) {
+    //     return (acc = true);
+    //   }
+    // }, false);
+    // let found = false;
+    // const exists = foundOrder[0].dishesOrdered.forEach((el) => {
+    //   if (el.dishType == dishId) {
+    //     found = true;
+    //   }
+    //   return found;
+    // });
+    let exists = false;
+    for (let i = 0; i < foundOrder[0].dishesOrdered.length; i++) {
+      console.log(
+        `foundOrder[${i}]: ${foundOrder[0].dishesOrdered[i].dishType} and Dishtype: ${dishId}==>${exists}`
+      );
+      if (foundOrder[0].dishesOrdered[i].dishType == dishId) {
+        exists = true;
       }
-    }, false);
+    }
+
     console.log("exists", exists);
 
     if (!exists) {
       Order.findByIdAndUpdate(
         foundOrder[0]._id,
-        { $addToSet: { dishesOrdered: { dishType: dishId, units: 1 } } },
+        {
+          $addToSet: { dishesOrdered: { dishType: dishId, units: 1 } },
+          $inc: { totalItems: 1 },
+        },
         { new: true }
       )
         .populate("dishesOrdered.dishType")
@@ -72,23 +92,30 @@ router.post("/addDishToOrder", (req, res, next) => {
             "newAndUpdatedOrder which didnt exist-->",
             newAndUpdatedOrder
           );
+
+          res.json(newAndUpdatedOrder);
         });
       return;
     }
+    console.log("just before updateOne");
     Order.updateOne(
       { _id: foundOrder[0]._id, "dishesOrdered.dishType": dishId },
-      { $inc: { "dishesOrdered.$.units": 1 } },
+      { $inc: { "dishesOrdered.$.units": 1, totalItems: 1 } },
       { new: true }
     )
-      .populate("dishesOrdered.dishType")
+
       // .populate("dishesOrdered")
-      .then(() => {
-        Order.find({ table: table }).then((newAndUpdatedOrder) => {
-          console.log(
-            "newAndUpdatedOrder which existed-->",
-            newAndUpdatedOrder
-          );
-        });
+      .then((result) => {
+        console.log("Result of updated", result);
+        Order.find({ table: table })
+          .populate("dishesOrdered.dishType")
+          .then((newAndUpdatedOrder) => {
+            console.log(
+              "newAndUpdatedOrder which existed-->",
+              newAndUpdatedOrder
+            );
+            res.json(newAndUpdatedOrder);
+          });
       });
   });
 });
